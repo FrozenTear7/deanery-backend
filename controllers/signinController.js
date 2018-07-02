6d0914665b4ef768bd25869d59f66d41512252db
FrozenTear7 <RainbowPablo7@gmail.com>
2018-07-02 20:24:06 +0200

    Fix simple authentication

--- controllers/signinController.js
+++ controllers/signinController.js
@@ -5,14 +5,14 @@ exports.postSignin = (req, res) => {
     if (err)
       res.status(500).send(err)
     else
-      User.find({}, (err, users) => {
-        if (err)
-          res.status(500).send(err)
-        else if (users.filter(dbUser => dbUser.index === user.index
-            && dbUser.password === user.password).length === 0)
-          res.status(404).send({message: 'User not found'})
-        else
-          res.send(user)
-      })
+    User.find({}, (err, users) => {
+      if (err)
+        res.status(500).send(err)
+      else if (users.filter(dbUser => dbUser.index === req.body.index
+          && dbUser.password === req.body.password).length === 0)
+        res.status(404).send({error: 'User not found'})
+      else
+        res.send(user[0])
+    })
   })
 }
